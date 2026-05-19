2896c3ad9eea9abafbcca0b3f3ff1ba4039a393d
NoAnt <noant1101@gmail.com>
2026-05-19 18:53:56 +0900

    에듀케이션 재추가 후 수정, 익스피리언스 삭제

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,14 +4,11 @@ import { Container } from 'reactstrap';
 import Head from 'next/head';
 import { NextSeo } from 'next-seo';
 import { Education } from '../component/education';
-import { Experience } from '../component/experience';
 import { Footer } from '../component/footer';
 import { Introduce } from '../component/introduce';
-import { Presentation } from '../component/presentation';
 import { Profile } from '../component/profile';
 import { Style } from '../component/common/Style';
 import Payload from '../payload';
-import { Article } from '../component/article';
 
 function Yosume() {
   return (
@@ -24,9 +21,6 @@ function Yosume() {
       <Container style={Style.global}>
         <Profile.Component payload={Payload.profile} />
         <Introduce.Component payload={Payload.introduce} />
-        <Experience.Component payload={Payload.experience} />
-        <Presentation.Component payload={Payload.presentation} />
-        <Article.Component payload={Payload.article} />
         <Education.Component payload={Payload.education} />
         <Footer.Component payload={Payload.footer} />
       </Container>
