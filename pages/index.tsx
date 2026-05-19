d7dd4f182689740a505bc6f3675d55dfe83ed5b8
NoAnt <noant1101@gmail.com>
2026-05-19 18:45:07 +0900

    스킬, 프로젝트, 오픈소스 삭제

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,11 +7,8 @@ import { Education } from '../component/education';
 import { Experience } from '../component/experience';
 import { Footer } from '../component/footer';
 import { Introduce } from '../component/introduce';
-import { OpenSource } from '../component/openSource';
 import { Presentation } from '../component/presentation';
 import { Profile } from '../component/profile';
-import { Project } from '../component/project';
-import { Skill } from '../component/skill';
 import { Style } from '../component/common/Style';
 import Payload from '../payload';
 import { Article } from '../component/article';
@@ -27,10 +24,7 @@ function Yosume() {
       <Container style={Style.global}>
         <Profile.Component payload={Payload.profile} />
         <Introduce.Component payload={Payload.introduce} />
-        <Skill.Component payload={Payload.skill} />
         <Experience.Component payload={Payload.experience} />
-        <Project.Component payload={Payload.project} />
-        <OpenSource.Component payload={Payload.openSource} />
         <Presentation.Component payload={Payload.presentation} />
         <Article.Component payload={Payload.article} />
         <Education.Component payload={Payload.education} />
